39bbcd3804f4d5c019ccd0e0fbcd666cb237a237
André Ekbom <andreekbom@live.se>
2018-06-25 23:03:51 +0200

    Change to use config isProduction

--- src/universal/createReduxStore.js
+++ src/universal/createReduxStore.js
@@ -2,14 +2,15 @@ import { createStore, applyMiddleware, compose } from 'redux'
 import createSagaMiddleware, { END } from 'redux-saga'
 import { createLogger } from 'redux-logger'
 import reduxState from '../app/redux/reducers'
+import config from '../configs'
 
 const loggerMiddleware = createLogger()
 const sagaMiddleware = createSagaMiddleware()
 
 export default function createReduxStore({ preloadedState, server } = {}) {
   let enhancer
-
-  if (process.env.NODE_ENV !== 'production' && !server) {
+  console.log('isProduction', config.isProduction)
+  if (!config.isProduction && !server) {
     /* eslint-disable no-underscore-dangle */
     const composeEnhancers = (global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ &&
       global.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
